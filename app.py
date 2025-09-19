c7dc6da56983fb016f4cd2a653e41d6d74ea37ec
Nfolio <139557367+Nfolio567@users.noreply.github.com>
2025-09-19 11:25:46 +0900

    databasev1

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_migrate import Migrate
+from sqlalchemy import false
 
+from database import db
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+migrate = Migrate(app, db)
 
 @app.route("/")
 def index():
@@ -15,4 +29,7 @@ def oeder():
     return render_template("order.html")
 
 if __name__ == "__main__":
+    '''with app.app_context():
+        db.create_all()'''
+
     app.run(port=6743, host='0.0.0.0', debug=True)
---
+++ database.py
@@ -0,0 +1,28 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Orders(db.Model):
+    __tablename__ = "orders"
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    is_provided = db.Column(db.Boolean)
+
+class Products(db.Model):
+    __tablename__ = "products"
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.Text)
+
+class Options(db.Model):
+    __tablename__ = "options"
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.Text)
+    products_id = db.Column(db.Integer, db.ForeignKey("products.id"))
+
+class OrderItems(db.Model):
+    __tablename__ = "order_items"
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    orderer_id = db.Column(db.Integer, db.ForeignKey("orders.id"))
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
+    option_id = db.Column(db.Integer, db.ForeignKey("options.id"))
+    quantity = db.Column(db.Integer)
+    price = db.Column(db.Numeric(10, 0))
