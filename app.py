25289b346ef5e73ce68b4ef3b13fe79720abb8a9
Nfolio <139557367+Nfolio567@users.noreply.github.com>
2025-09-20 10:12:47 +0900

    prototype; return all orders

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, jsonify
 from flask_migrate import Migrate
-from sqlalchemy import false
-from flask_socketio import SocketIO
-from database import db
+from flask_socketio import SocketIO, emit
+from database import db, Orders
 from dotenv import load_dotenv
 import os
+from sqlalchemy import event
 
 load_dotenv()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JSON_AS_ASCII"] = False
 
 socketio = SocketIO(app)
 
@@ -20,22 +21,40 @@ migrate = Migrate(app, db)
 
 @app.route("/")
 def index():
-    return redirect(url_for("order_list"))
+  return redirect(url_for("order_list"))
 
 @app.route("/order-list")
 def order_list():
-    return render_template("order_list.html")
+  return render_template("order_list.html")
 
 @app.route("/order")
 def order():
-    return render_template("order.html")
+  return render_template("order.html")
 
 @app.route("/api/list")
 def return_list():
-    return 200
+  orders = {}
+  for i in range(1, int(os.getenv("PLATE_NUM"))):
+    order = Orders(id=i)
+    order_items = order.items  # 一人の注文の品の詳細取得
+    if not order.is_provided:  # 注文されてなかったらスキップ
+      continue
+    for j in order_items:  # 注文の詳細たちを処理
+      options = [k.name for k in j.product.options]  # 配列に変換
+      orders[str(i)] = {"ordererId": j.orderer_id, "item": j.product, "options": options, "quantity": j.quantity, "price": j.price}
+
+  return jsonify({orders})
+
+@event.listens_for(Orders, "after_update")
+def new_order(_, __, target):
+  all_order = []
+  order_items = target.items
+  for i in order_items:
+    all_order.append(i)
+  emit("newOrder", all_order)
 
 if __name__ == "__main__":
-    '''with app.app_context():
-        db.create_all()'''
+  '''with app.app_context():
+      db.create_all()'''
 
-    socketio.run(app, port=6743, host='0.0.0.0', debug=True)
+  socketio.run(app, port=6743, host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -3,30 +3,28 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Orders(db.Model):
-    __tablename__ = "orders"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    is_provided = db.Column(db.Boolean)
-    items = db.relationship("OrderItems")
+  __tablename__ = "orders"
+  id = db.Column(db.Integer, primary_key=True, unique=True)
+  is_provided = db.Column(db.Boolean)
+  items = db.relationship("OrderItems")
 
 class Products(db.Model):
-    __tablename__ = "products"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    name = db.Column(db.Text)
-    options = db.relationship("Options")
+  __tablename__ = "products"
+  id = db.Column(db.Integer, primary_key=True, unique=True)
+  name = db.Column(db.Text)
+  options = db.relationship("Options")
 
 class Options(db.Model):
-    __tablename__ = "options"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    name = db.Column(db.Text)
-    products_id = db.Column(db.Integer, db.ForeignKey("products.id"))
-    products = db.relationship("Products", back_populates="options")
+  __tablename__ = "options"
+  id = db.Column(db.Integer, primary_key=True, unique=True)
+  name = db.Column(db.Text)
+  products_id = db.Column(db.Integer, db.ForeignKey("products.id"))
 
 class OrderItems(db.Model):
-    __tablename__ = "order_items"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    orderer_id = db.Column(db.Integer, db.ForeignKey("orders.id"))
-    product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
-    option_id = db.Column(db.Integer, db.ForeignKey("options.id"))
-    quantity = db.Column(db.Integer)
-    price = db.Column(db.Numeric(10, 0))
-    order = db.relationship("Orders", back_populates="items")
+  __tablename__ = "order_items"
+  id = db.Column(db.Integer, primary_key=True, unique=True)
+  orderer_id = db.Column(db.Integer, db.ForeignKey("orders.id"))
+  product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
+  product = db.relationship("Products")
+  quantity = db.Column(db.Integer)
+  price = db.Column(db.Numeric(10, 0))
